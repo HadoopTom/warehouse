f005dc889edf1da7c8d30bd3598b1f71bc042d9a
derpcube <68118715+derpcube@users.noreply.github.com>
2020-07-10 22:26:08 +0600

    2 commit. The most I'll do for today bro. Added cycle to accept string.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <deque>
+
+using namespace std;
 
 int main() {
-    int a;
+    string a,b;
+    deque <int> p;
+    cin>>a;
+    while (cin>>b) {
+
+    }
     return 0;
 }
