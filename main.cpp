f30f10c59e6909a6520843df34a0652f56c123ef
derpcube <68118715+derpcube@users.noreply.github.com>
2020-07-10 22:24:38 +0600

    1 commit.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 
 int main() {
-// my first try
-// Aktan's change 
-
-//This is my commit.
-//im in anew brench called slave.
-//I'm helping daddy with a new branch project
-//  shitty shit shit
+    int a;
     return 0;
 }
