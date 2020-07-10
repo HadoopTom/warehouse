421d8117d643598bad46854efcccb6263a7b58f9
derpcube <68118715+derpcube@users.noreply.github.com>
2020-07-10 23:33:02 +0600

    3 commit. Added plan for FIFO/

--- main.cpp
+++ main.cpp
@@ -3,12 +3,26 @@
 
 using namespace std;
 
+struct stuff{
+    int amount;
+    int price;
+};
+
 int main() {
     string a,b;
-    deque <int> p;
+    stuff p;
+    deque<int> lol;
     cin>>a;
+    int LIFO,FIFO,Med;
     while (cin>>b) {
-
+        //FIFO
+       if (b == "E") {
+           continue;
+       }
+       cin>>p.amount>>p.price;
+       if (b == "B") {
+           FIFO+=p.amount*p.price;
+       }
     }
     return 0;
 }
